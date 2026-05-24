d9b543aaab2f200ffabe33e8e86f60b97e0c177b
Yunus Emre Eren <y.emre.eren19@gmail.com>
2026-05-24 19:13:54 +0300

    Zaman yonetimi icin metotlar olusturuldu.

--- include/Timer.h
+++ include/Timer.h
@@ -2,14 +2,21 @@
 #define TIMER_H
 
 #pragma once
+#include <SDL2/SDL.h>
 
 class Timer
 {
 public:
     Timer();
+    void baslat();
+    void durdur();
+    Uint32 gecenSure();
+    bool calisiyorMu();
     
 
 private:
+   Uint32 baslangicZamani;
+   bool calisiyor;
 
 };
 
--- src/Timer.cpp
+++ src/Timer.cpp
@@ -2,6 +2,7 @@
 
 Timer::Timer()
 {
-
+    
 }
+
 
