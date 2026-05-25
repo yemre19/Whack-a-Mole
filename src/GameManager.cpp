cdd5188174ed21fb7aa28ae339c82b4ac2438a82
Yunus Emre Eren <y.emre.eren19@gmail.com>
2026-05-25 10:12:16 +0300

    GameManager.cpp icinde baslat fonksiyonu tanimlandi.

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -21,3 +21,30 @@ OyunDurumu GameManager::getDurum()
     return this->mevcutDurum;
 }
 
+void GameManager::baslat(int sureSecimiSaniye,int zorlukSecimi)
+{
+ secilenSure = sureSecimiSaniye * 100;
+
+ if (zorlukSecimi == 1)
+ {
+    secilenHiz = 4;
+    secilenSpawnSuresi=2000;
+ }
+
+  if (zorlukSecimi == 2)
+ {
+    secilenHiz = 7;
+    secilenSpawnSuresi=1000;
+ }
+
+  if (zorlukSecimi == 3)
+ {
+    secilenHiz = 11;
+    secilenSpawnSuresi=500;
+ }
+ 
+
+ mevcutDurum = OyunDurumu::OYUN_ICI;
+ oyunTimer.baslat();
+ spawnTimer.baslat();
+}
