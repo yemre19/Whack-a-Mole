b31598ba3f8587d44e20a4d9767d0124f437f1ca
Yunus Emre Eren <y.emre.eren19@gmail.com>
2026-05-25 10:02:35 +0300

    GameManager.cpp icinde gerekli kutuphaneler eklendi.

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,7 +1,19 @@
 #include "GameManager.h"
+#include <iostream>
+#include <string>
 
 GameManager::GameManager()
 {
 
+}
+
+void GameManager::setDurum(OyunDurumu yeniDurum)
+{
+    mevcutDurum=yeniDurum;
+}
+
+OyunDurumu GameManager::getDurum()
+{
+    return this->mevcutDurum;
 }
 
